070e0b1036dbb9d88b8c81c88c594d76743a09e2
ChrisChrisLoLo <sporewoh@gmail.com>
2018-04-14 18:52:30 -0600

    (Should) now filter out NSFW material

--- imageGrab.js
+++ imageGrab.js
@@ -27,13 +27,15 @@ module.exports = {
 					while(imagesGotten < imageAmount){
 						//Check if post is an image post
 						if (redditObj.data.children[i].data.preview.images[0]){
-							imagePost = redditObj.data.children[i].data.preview.images[0].source.url
-							console.log(checkExtension(imagePost))
-							//Check if post is a jpg post, and not a gifv or gif post. For now only jpgs are collected
-							//Though this could probably be expanded in the future.
-							if (checkExtension(imagePost) === "jpg"){
-								linkToImage(redditObj.data.children[i].data.preview.images[0].source.url,imagesGotten,"jpg")
-								imagesGotten += 1
+							//filter out NSFW materials
+							if (redditObj.data.children[i].data.over_18 === false){
+								imagePost = redditObj.data.children[i].data.preview.images[0].source.url
+								console.log(checkExtension(imagePost))
+								//Check if post is a jpg post, and not a gifv or gif post.
+								if (checkExtension(imagePost) === "jpg"){
+									linkToImage(redditObj.data.children[i].data.preview.images[0].source.url,imagesGotten,"jpg")
+									imagesGotten += 1
+								}
 							}
 						}
 						i++;
